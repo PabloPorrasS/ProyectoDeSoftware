407bbb8e0da9d2458bc49b01f2219d28b5a05f14
Pablo Porras <josexpablox@outlook.es>
2019-08-05 00:18:46 -0600

    Cargar tabla

---
+++ Proyecto Definitivo/ProyectoDefinitivo/Facturas.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Facturas
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Splitter1 = New System.Windows.Forms.Splitter()
+        Me.ButtonInventory = New System.Windows.Forms.Button()
+        Me.ButtonBill = New System.Windows.Forms.Button()
+        Me.ButtonDelivery = New System.Windows.Forms.Button()
+        Me.ButtonTrends = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Id_Profile = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Splitter1
+        '
+        Me.Splitter1.BackColor = System.Drawing.Color.Green
+        Me.Splitter1.Location = New System.Drawing.Point(0, 0)
+        Me.Splitter1.Name = "Splitter1"
+        Me.Splitter1.Size = New System.Drawing.Size(265, 450)
+        Me.Splitter1.TabIndex = 15
+        Me.Splitter1.TabStop = False
+        '
+        'ButtonInventory
+        '
+        Me.ButtonInventory.BackColor = System.Drawing.Color.Green
+        Me.ButtonInventory.FlatAppearance.BorderSize = 0
+        Me.ButtonInventory.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ButtonInventory.Font = New System.Drawing.Font("Arial Narrow", 16.0!)
+        Me.ButtonInventory.Location = New System.Drawing.Point(38, 88)
+        Me.ButtonInventory.Name = "ButtonInventory"
+        Me.ButtonInventory.Size = New System.Drawing.Size(165, 44)
+        Me.ButtonInventory.TabIndex = 16
+        Me.ButtonInventory.Text = "Inventario"
+        Me.ButtonInventory.UseVisualStyleBackColor = False
+        '
+        'ButtonBill
+        '
+        Me.ButtonBill.BackColor = System.Drawing.Color.Green
+        Me.ButtonBill.FlatAppearance.BorderSize = 0
+        Me.ButtonBill.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ButtonBill.Font = New System.Drawing.Font("Arial Narrow", 16.0!)
+        Me.ButtonBill.Location = New System.Drawing.Point(38, 169)
+        Me.ButtonBill.Name = "ButtonBill"
+        Me.ButtonBill.Size = New System.Drawing.Size(165, 44)
+        Me.ButtonBill.TabIndex = 17
+        Me.ButtonBill.Text = "Facturas"
+        Me.ButtonBill.UseVisualStyleBackColor = False
+        '
+        'ButtonDelivery
+        '
+        Me.ButtonDelivery.BackColor = System.Drawing.Color.Green
+        Me.ButtonDelivery.FlatAppearance.BorderSize = 0
+        Me.ButtonDelivery.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ButtonDelivery.Font = New System.Drawing.Font("Arial Narrow", 16.0!)
+        Me.ButtonDelivery.Location = New System.Drawing.Point(38, 262)
+        Me.ButtonDelivery.Name = "ButtonDelivery"
+        Me.ButtonDelivery.Size = New System.Drawing.Size(165, 44)
+        Me.ButtonDelivery.TabIndex = 18
+        Me.ButtonDelivery.Text = "Entregas"
+        Me.ButtonDelivery.UseVisualStyleBackColor = False
+        '
+        'ButtonTrends
+        '
+        Me.ButtonTrends.BackColor = System.Drawing.Color.Green
+        Me.ButtonTrends.FlatAppearance.BorderSize = 0
+        Me.ButtonTrends.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ButtonTrends.Font = New System.Drawing.Font("Arial Narrow", 16.0!)
+        Me.ButtonTrends.Location = New System.Drawing.Point(38, 350)
+        Me.ButtonTrends.Name = "ButtonTrends"
+        Me.ButtonTrends.Size = New System.Drawing.Size(165, 44)
+        Me.ButtonTrends.TabIndex = 19
+        Me.ButtonTrends.Text = "Tendencias"
+        Me.ButtonTrends.UseVisualStyleBackColor = False
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(271, 0)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.Size = New System.Drawing.Size(528, 450)
+        Me.DataGridView1.TabIndex = 22
+        '
+        'Id_Profile
+        '
+        Me.Id_Profile.AutoSize = True
+        Me.Id_Profile.BackColor = System.Drawing.Color.Green
+        Me.Id_Profile.Location = New System.Drawing.Point(12, 9)
+        Me.Id_Profile.Name = "Id_Profile"
+        Me.Id_Profile.Size = New System.Drawing.Size(39, 13)
+        Me.Id_Profile.TabIndex = 23
+        Me.Id_Profile.Text = "Label1"
+        '
+        'Facturas
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Id_Profile)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.ButtonTrends)
+        Me.Controls.Add(Me.ButtonDelivery)
+        Me.Controls.Add(Me.ButtonBill)
+        Me.Controls.Add(Me.ButtonInventory)
+        Me.Controls.Add(Me.Splitter1)
+        Me.Name = "Facturas"
+        Me.Text = "Facturas"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Splitter1 As Splitter
+    Friend WithEvents ButtonInventory As Button
+    Friend WithEvents ButtonBill As Button
+    Friend WithEvents ButtonDelivery As Button
+    Friend WithEvents ButtonTrends As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Id_Profile As Label
+End Class
---
+++ Proyecto Definitivo/ProyectoDefinitivo/Facturas.vb
@@ -0,0 +1,63 @@
+﻿Imports System.Data.SqlClient
+Public Class Facturas
+    Public rowOfGridview As Integer
+
+    Dim ConnectionString As String = "Data Source=SP-LA-LAB9-13;Initial Catalog=Inventory;Integrated Security=True"
+
+    Private Sub Facturas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        LoadGridView()
+
+    End Sub
+
+    Sub LoadGridView()
+
+        Dim Id As Integer
+
+
+        Dim Connection As New SqlConnection(ConnectionString)
+
+
+        Dim commandselect As SqlCommand
+
+
+        Dim SelectQuery As String
+        SelectQuery = "Select Id FROM Profile WHERE Email ='" & Login.TextBoxEmail.Text & "'"
+        commandselect = New SqlCommand(SelectQuery, Connection)
+        Connection.Open()
+
+        Dim reader As SqlDataReader
+        reader = commandselect.ExecuteReader()
+
+        If reader.HasRows Then
+            reader.Read()
+            Id = reader.GetInt32(0)
+
+        End If
+
+        Id_Profile.Text = Id
+
+        Connection.Close()
+
+        Dim Bills As New DataTable
+
+        SelectQuery = "SELECT  Id_Profile 'Id_Profile', Id_Product 'Id_Product', Fecha 'Date', TotalPagado 'Total Pagado' FROM Bills WHERE Id_Profile ='" & Id & "'"
+        commandselect = New SqlCommand(SelectQuery, Connection)
+        Dim dataAdapter As New SqlDataAdapter(commandselect)
+        dataAdapter.Fill(Bills)
+        DataGridView1.DataSource = Bills
+
+        DataGridView1.Columns(0).Visible = False
+        DataGridView1.Columns(1).Visible = False
+
+        DataGridView1.AllowUserToAddRows = False
+        DataGridView1.AllowUserToDeleteRows = False
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+        rowOfGridview = DataGridView1.CurrentRow.Index
+
+
+    End Sub
+End Class
