9da1cdc7a6f4cfd73ef4420a94609549367bb197
leo <Laboratorio@SP-LA-LAB9-14>
2019-08-13 14:05:42 -0600

    bfx<bdf

--- Proyecto Definitivo/ProyectoDefinitivo/My Project/Application.Designer.vb
+++ Proyecto Definitivo/ProyectoDefinitivo/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.ProyectoDefinitivo.HomeCliente
+            Me.MainForm = Global.ProyectoDefinitivo.Login
         End Sub
     End Class
 End Namespace
