c40c0aa5e9f2b1bcabc1d2fbce05b851eff46231
Gabriel <gabrielgranadosc@gmail.com>
2019-08-18 23:14:09 -0600

    Se puede poner numero en los textboxes de Ubicacion (en el commit anterior no se podia)

--- Proyecto Definitivo/ProyectoDefinitivo/EditarPerfil.vb
+++ Proyecto Definitivo/ProyectoDefinitivo/EditarPerfil.vb
@@ -135,16 +135,4 @@ Public Class EditarPerfil
             e.Handled = False
         Else e.Handled = True
         End If
-    End Sub
-
-    Private Sub TextBoxUbication_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBoxUbication.KeyPress
-        If Char.IsLetter(e.KeyChar) Then
-            e.Handled = False
-        ElseIf Char.IsControl(e.KeyChar) Then
-            e.Handled = False
-        ElseIf Char.IsSeparator(e.KeyChar) Then
-            e.Handled = False
-        Else e.Handled = True
-        End If
-    End Sub
-   
+    End Sub 
--- Proyecto Definitivo/ProyectoDefinitivo/FormularioDeRegistro.vb
+++ Proyecto Definitivo/ProyectoDefinitivo/FormularioDeRegistro.vb
@@ -94,15 +94,4 @@ Public Class FormularioDeRegistro
         Else e.Handled = True
         End If
     End Sub
-
-    Private Sub TextBoxUbication_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBoxUbication.KeyPress
-        If Char.IsLetter(e.KeyChar) Then
-            e.Handled = False
-        ElseIf Char.IsControl(e.KeyChar) Then
-            e.Handled = False
-        ElseIf Char.IsSeparator(e.KeyChar) Then
-            e.Handled = False
-        Else e.Handled = True
-        End If
-    End Sub
 End Class
